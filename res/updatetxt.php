651d2f99ec28dd4212772d95241d2c26189d9d97
YiWei Zheng <yiweizhe@usc.edu>
2013-01-21 14:55:41 -0800

    hk

---
+++ res/updatetxt.php
@@ -0,0 +1,20 @@
+<?
+
+$files = array('urls.txt','usrnames.txt','programs.txt','directory.txt','source.txt','package.txt','get.txt');
+
+foreach($files as $filename) {
+    $file = fopen($filename,'r');
+
+    $lines = 0;
+    $array = array();
+    while($line = fgets($file)) {
+        $array[] = $line;
+        $lines++;
+    }
+    $array[0] = ($lines-1)."\n";
+    $file = fopen($filename,'w');
+    fwrite($file,implode($array));
+
+}
+
+?>
